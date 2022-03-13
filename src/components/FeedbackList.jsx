7ba66c69907034e2817c0e67b659d8f9d8867338
Chudy1 <98897666+Chudy1@users.noreply.github.com>
2022-03-13 09:31:53 +0100

    Update FeedbackList.jsx

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
